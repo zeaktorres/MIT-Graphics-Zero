7e698fb68bd2fd1cde4ddd0b24e4ecd2f1e195d5
zeaktorres <torreszeaks@gmail.com>
2023-11-20 12:13:36 -0800

    Adding color changes.

---
+++ vecmath.h
@@ -0,0 +1,13 @@
+#ifndef VECMATH_H
+#define VECMATH_H
+
+#include "Matrix2f.h"
+#include "Matrix3f.h"
+#include "Matrix4f.h"
+#include "Quat4f.h"
+#include "Vector2f.h"
+#include "Vector3f.h"
+#include "Vector4f.h"
+#include "util.h"
+
+#endif // VECMATH_H
